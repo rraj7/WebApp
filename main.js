20fb0f3cf209cfde846f0d933e0e56f4e0082833
Rishi Raj <rraj7@uic.edu>
2020-05-16 03:46:13 -0500

    Added main.js file

---
+++ main.js
@@ -0,0 +1,59 @@
+document.getElementById('issueInputForm').addEventListener('submit',saveIssue);
+
+
+function saveIssue(e){
+	var issueDesc = document.getElementById('issueDescInput').Value;
+	var issueSeverity = document.getElementById('issueSeverityInput').Value;
+	var issueAssignedTo = document.getElementById('issueAssignedToInput').Value;
+	var issueId = chance.guid();
+	var issuesStatus = 'Open';
+
+	var issue = {
+		id: issueId,
+		description: issueDesc,
+		severity: issueSeverity,
+		assignedTo: issueAssignedTo,
+		status: issuesStatus
+	}
+
+	if (localStorage.getItem('issues')==null){
+		var issues = [];
+		issues.push(issue);
+		localStorage.setItem('issues',JSON.stringify(issues));
+
+	}else {
+		var issues = JSON.parse(localStorage.getItem('issues'));
+		issues.push(issue);
+		localStorage.setItem('issues',JSON.stringify(issues));
+
+	}
+
+	document.getElementById('issueInputForm').reset();
+	fetchIssues();
+	e.preventDefault();
+}
+
+function fetchIssues(){
+	var issues = JSON.parse(localStorage.getItem('issues'));
+	var issuesList = document.getElementById('issuesList');
+
+	issuesList.innerHTML = "";
+	for (var i = 0;i <issues.length;i++){
+		var id = issues[i].id;
+		var desc = issues[i].description;
+		var severity = issues.issues[i].assignedTo;
+		var status = issues[i].status;
+
+		issuesList.innerHTML += '<div class="well">'+
+								'<h6>Issue ID: '+id+ '</h6>'+
+								'<p><span class = "label label-info">'+status+'</span></p>'+
+								'<h3>'+desc+'</h3>'+
+								'<p><span class = "glyphicon glyphicon-time"></span>' + severity + '</p>'+
+								'<p><span class = "glyphicon glyphicon-user"></span>' + severity + '</p>'+
+								'<a href="#" onclick="getStatusClosed(\''+id+'\') class= "btn btn-warning">Close</a>'+
+								'<a href="#" onclick ="deleteIssue(\''+id+'\')" class= "btn btn-danger">Delete</a>'+
+								'</div>';
+
+	}
+
+}
